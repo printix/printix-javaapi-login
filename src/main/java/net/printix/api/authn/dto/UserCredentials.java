2b86579c3599f5570e90e2f07a4e0e82272ea45d
Claus Nielsen <claus@printix.net>
2018-11-05 14:45:16 +0100

    Use lombok for UserCredentials

--- src/main/java/net/printix/api/authn/dto/UserCredentials.java
+++ src/main/java/net/printix/api/authn/dto/UserCredentials.java
@@ -1,52 +1,20 @@
 package net.printix.api.authn.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCredentials {
 
 	private String username;
 	private String password;
 	private String totpSecret;
 	
-	public UserCredentials() {
-	}
-	
-	public UserCredentials(String username, String password, String totpSecret) {
-		this.username = username;
-		this.password = password;
-		this.totpSecret = totpSecret;
-	}
-
 	public UserCredentials(String username, String password) {
 		this(username, password, null);
 	}
-
-	public String getUsername() {
-		return username;
-	}
-	
-	
-	public void setUsername(String userName) {
-		this.username = userName;
-	}
-	
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
-	public String getTotpSecret() {
-		return totpSecret;
-	}
-	
-	
-	public void setTotpSecret(String mfaToken) {
-		this.totpSecret = mfaToken;
-	}
-	
 
 }
